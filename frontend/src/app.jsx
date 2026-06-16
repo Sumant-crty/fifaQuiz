e42f6bf37e77dbac5b50c069db4ededf52380444
Sumant-crty <162594691+Sumant-crty@users.noreply.github.com>
2026-06-16 19:13:21 +0530

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,1 +1,28 @@
+import { useEffect, useState } from "react";
 
+function App() {
+  const [questions, setQuestions] =
+    useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://your-backend.onrender.com/api/questions"
+    )
+      .then(res => res.json())
+      .then(data => setQuestions(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>FIFA Quiz</h1>
+
+      {questions.map((q, i) => (
+        <div key={i}>
+          <h3>{q.question}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
